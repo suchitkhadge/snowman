f8b41b28a3687e058da16579695c5ce474160969
skhadge <suchit.khadge@gmail.com>
2022-10-07 05:47:39 -0700

    Suchit-final checklist go through

--- src/main/java/com/game/controller/Guesses.java
+++ src/main/java/com/game/controller/Guesses.java
@@ -4,27 +4,28 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
+/*
+*  Method to check to see if the user character is in the hidden word or not. It uses the while loop
+* to validate the input and ask user to enter again if the input is not valid.
+* */
 public class Guesses {
 
-  static boolean getPlayerGuess(Scanner keyboard, String word,
-      List<Character> playerGuesses, Set<String> usedCharacterSet) {
+
+  public static boolean isGuessLetterInWord(Scanner keyboard, String word, List<Character> playerGuesses, Set<String> usedCharacterSet) {
     System.out.println("**********************************");
     System.out.println("Please enter a letter:");
     String letterGuess = keyboard.nextLine().toLowerCase();
     char charGuess;
     charGuess = letterGuess.charAt(0);
     while ((usedCharacterSet.contains(letterGuess)) || (!Character.isAlphabetic(charGuess))) {
-      System.out.println("Invalid attempt. Please try again");
+      System.out.println("Invalid attempt. Not a letter or letter already used. Please try again");
       letterGuess = keyboard.nextLine();
       charGuess = letterGuess.charAt(0);
     }
-
     usedCharacterSet.add(letterGuess);
     System.out.println(usedCharacterSet);
     playerGuesses.add(letterGuess.charAt(0));
-    return word.contains(letterGuess);
+      return word.contains(letterGuess);
   }
 
 }
-
-
